598550037ff055b06470693a36a1409dcd581bfb
banyuner <59797481+banyuner@users.noreply.github.com>
2023-07-23 16:12:03 +0800

    Update reddit.js

--- Scripts/reddit.js
+++ Scripts/reddit.js
@@ -12,10 +12,10 @@ if (body.data) {
     body.data.subredditInfoByName.elements.edges = body.data.subredditInfoByName.elements.edges.filter(i => i && i.node && (i.node.__typename !== "AdPost"))
   } else if (body.data.home && body.data.home.elements && body.data.home.elements.edges) {
     body.data.home.elements.edges = body.data.home.elements.edges.filter(i => i && i.node && (i.node.__typename !== "AdPost"))
-  } else if(body.data.subredditsInfoByNames){
+  } else if (body.data.subredditsInfoByNames) {
     body.data.subredditsInfoByNames = body.data.subredditsInfoByNames.map(i => ({...i, isNsfw: false}))
   } else {
     modified = false
   }
 }
-$done(modified ? { body: JSON.stringify(body) } : {})
+$done(modified ? {body: JSON.stringify(body)} : {})
